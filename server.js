281fa3147c91a5ca1986be819983e06623454abe
biswabijayeeninayak <139632459+biswabijayeeninayak@users.noreply.github.com>
2025-09-19 17:17:20 +0530

    swagger setup is done

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express=require('express');
 const connectDB=require('./src/config/db')
 const morgan=require('morgan');
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express");
+const options = require('./src/config/swagger');
+const healthRoute=require('./src/routes/health');
 const PORT=process.env.PORT || 5000;
 const app=express();
 require('dotenv').config();
 app.use(morgan('dev'));
 app.use(express.json());
-app.get("/health",(req,res)=>{
-    res.send("API is running");
-})
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+ 
+app.use('/api',healthRoute);
 app.listen(PORT,async()=>{
     try{
         await connectDB();
