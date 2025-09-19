f2f4600afbc88721589282bceb5778a83423078e
biswabijayeeninayak <139632459+biswabijayeeninayak@users.noreply.github.com>
2025-09-19 16:45:10 +0530

    basic helth checkup is done

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const connectDB=require('./src/config/db')
+const morgan=require('morgan');
 const PORT=process.env.PORT || 5000;
 const app=express();
 require('dotenv').config();
+app.use(morgan('dev'));
 app.use(express.json());
+app.get("/health",(req,res)=>{
+    res.send("API is running");
+})
 app.listen(PORT,async()=>{
     try{
         await connectDB();
